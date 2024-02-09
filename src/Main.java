0f0e1b500f8517a5ceaa1ecf84ba1cafbade4dce
kregoric <75627074+isicisawsum@users.noreply.github.com>
2024-02-09 18:40:06 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
+import java.util.Scanner; //importing Scanner
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a random number: "); //asking user to input number 1
+        double num1 = in.nextDouble(); //defining variable num2
+        System.out.println("Choose another random number: "); //asking user to input number 2
+        if (in.hasNextDouble()) //if/then to determine if user entered a number
+        {
+            double num2 = in.nextDouble(); //defining variable num2
+            if (num1 > num2) //number 1 that user inputted is greater than number 2
+            {
+                System.out.println(num1 + " is greater than " + num2); //telling user num1 greater than num2
+            }
+            else if (num1 < num2) //number 1 that user inputted is less than number 2
+            {
+                System.out.println(num1 + " is less than " + num2); //telling user num1 less than num2
+            }
+            else
+            {
+                System.out.println(num1 + " is the same as " + num2); //telling user numbers are equal
+            }
+        }
+        else
+        {
+            System.out.println("That’s not a number"); //telling user that they didn't input a number
         }
     }
 }
